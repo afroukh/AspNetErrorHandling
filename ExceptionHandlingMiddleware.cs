1127f0ed1e8bb695097dc0c44b5b60ac02220432
Marouane Afroukh <m.afroukh@gmail.com>
2024-10-01 18:53:32 -0400

    initial commit

---
+++ ExceptionHandlingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+namespace HandlingError
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var result = Newtonsoft.Json.JsonConvert.SerializeObject(new { error = exception.Message });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
